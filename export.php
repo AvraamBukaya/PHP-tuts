bdfcb5bd32b883703ef90d3873ff788b0e26d51f
Avraham <avrahambukaya@gmail.com>
2021-02-13 16:03:52 +0200

    'require

---
+++ export.php
@@ -0,0 +1,3 @@
+<?php
+$persons = ['shaun', 'ray', 'tim'];
+echo $persons[0] . '<br>';
---
+++ require_include.php
@@ -0,0 +1,33 @@
+<?php
+
+/* 
+allow us to import piece of code from on module to anthor.
+for instance if we want to load temple in diffrent pages, 
+with the keyword include or require we can do so. 
+help us to avoid repeation od code etc. 
+*/
+
+// include('export.php');
+// require('export.php') 
+
+# or
+include 'export.php';
+require 'export.php';
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorials</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
